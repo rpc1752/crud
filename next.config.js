e7c18b950b2cfe7ff8857cb7ece72ef978bc7c7c
rpc1752 <rpc1752@gmail.com>
2025-03-22 21:39:13 +0530

    Add Netlify configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
+  reactStrictMode: true,
+  // Add these settings for better Netlify compatibility
+  swcMinify: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
